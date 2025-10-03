dd2931203aaafb2b652a3ea73f9c5ec695925822
mohd shakoor <samsuddin.s924@gmail.com>
2025-10-03 19:13:21 +0530

    d13

--- assets/assets.js
+++ assets/assets.js
@@ -98,7 +98,7 @@ export const serviceData = [
     { icon: assets.web_icon, title: 'Web design', description: 'Web development is the process of building, programming...', link: '' },
     { icon: assets.mobile_icon, title: 'Mobile app', description: 'Mobile app development involves creating software for mobile devices...', link: '' },
     { icon: assets.ui_icon, title: 'UI/UX design', description: 'UI/UX design focuses on creating a seamless user experience...', link: '' },
-    { icon: assets.graphics_icon, title: 'Graphics design', description: 'Creative design solutions to enhance visual communication...', link: '' },
+    { icon: assets.graphics_icon, title: 'Skilled in leveraging Python’s rich ecosystem (NumPy, Pandas, Scikit-learn, TensorFlow, PyTorch) to develop, train, and optimize machine learning models', link: '' },
 ]
 
 export const infoList = [
